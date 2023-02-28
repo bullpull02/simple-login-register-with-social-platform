301e34fcb38a3d8da5451311533e41b885c68728
bullpull02 <mykolazbudchak@gmail.com>
2023-02-28 23:12:26 +0200

    [update] route reference change

--- backend-service/routers/user.route.js
+++ backend-service/routers/user.route.js
@@ -2,5 +2,5 @@ const express = require('express');
 const { register, defaultLogin } = require('../controllers/userController');
 const user = express.Router();
 user.post('/signup',register);
-user.post('/sigin',defaultLogin);
+user.post('/login',defaultLogin);
 module.exports = user;
