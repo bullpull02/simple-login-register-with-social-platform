6f549e50aeba4e2735e9669d59d158eecea454a8
bullpull02 <mykolazbudchak@gmail.com>
2023-02-28 23:51:24 +0200

    [add] OAuthClient functionality

--- backend-service/controllers/userController.js
+++ backend-service/controllers/userController.js
@@ -1,8 +1,12 @@
 const User = require("../models/users");
 const jwt = require("jsonwebtoken");
 const lodash = require("lodash");
-const hashPassword = require('../utils/hash')
+const hashPassword = require("../utils/hash");
 
+const { OAuth2Client } = require("google-auth-library");
+const uuid = require("uuid");
+const client = new OAuth2Client("your-client-id");
+const redirectUri = "http://localhost:8000/google/callback";
 const register = async (req, res) => {
   try {
     const userEach = await User.findOne({ email: req.body.email });
@@ -18,7 +22,7 @@ const register = async (req, res) => {
       res.json({ success: "ok", user: finalUser });
     }
   } catch (error) {
-    console.log(error);
+    return error;
   }
 };
 
@@ -44,4 +48,27 @@ const defaultLogin = async (req, res) => {
     return res.json({ status: "error", retrieved_user: false });
   }
 };
-module.exports = { register, defaultLogin };
+const googleLogin = async () => {
+  const state = uuid.v4();
+  const url = client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: "email profile openid",
+    state,
+    redirect_uri: redirectUri,
+  });
+
+  res.redirect(url);
+};
+const googleCallback = async () => {
+  const { code, state } = req.query;
+  const { tokens } = await client.getToken(code);
+
+  // Verify the state to prevent CSRF attacks
+  if (state !== req.session.state) {
+    res.status(401).send("Invalid state parameter");
+    return;
+  }
+  req.session.tokens = tokens;
+};
+module.exports = { register, defaultLogin , googleLogin,googleCallback};
