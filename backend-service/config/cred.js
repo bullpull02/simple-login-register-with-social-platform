1a55ee34a5c87eb2237c6503d0e09d58977c5b27
bullpull02 <mykolazbudchak@gmail.com>
2023-03-01 09:11:20 +0200

    [add] --credentials for testing

---
+++ backend-service/config/cred.js
@@ -0,0 +1,11 @@
+const discord = {
+    client_id : "1080379668143804418",
+    client_secret : "o0jOmoSaTREmsJVFLUWH_FBMb-HQ9_Oy",
+    callback_url : "http://localhost:8000/api/v1/user/sessions/oauth/discord"
+}
+const google = {
+    client_id : "42056636488-5pcnca03i14e5s0747qfqjbiddluf1ng.apps.googleusercontent.com",
+    client_secret : "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",
+    callback_url : "http://localhost:8000/api/v1/user/sessions/oauth/google"
+}
+module.exports = {discord,google}
