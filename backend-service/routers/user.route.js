4ecd4cd1b2d4e65212adc36bb278f5d1f4acdd61
bullpull02 <mykolazbudchak@gmail.com>
2023-03-01 08:20:49 +0200

    [update] route addition

--- backend-service/routers/user.route.js
+++ backend-service/routers/user.route.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const { register, defaultLogin, googleLogin, googleCallback } = require('../controllers/userController');
+const express = require("express");
+const passport = require("passport");
+const {
+  register,
+  defaultLogin
+} = require("../controllers/userController");
 const user = express.Router();
-user.post('v1/user/signup',register);
-user.post('v1/user/login',defaultLogin);
-user.get('/google/login',googleLogin);
-user.get('/google/callback',googleCallback)
-user.gte
+user.post("/signup", register);
+user.post("/login", defaultLogin);
+user.get(
+  "/auth/google/login",
+  passport.authenticate("google", { scope: ["email", "profile", "openid"] })
+);
+user.get(
+  "/sessions/oauth/google",
+  passport.authenticate("google", { failureRedirect: "/auth/google/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
 module.exports = user;
