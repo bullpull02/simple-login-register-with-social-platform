6d19ab0e9c77fc104522e0fe5d8fe677948bb6a8
bullpull02 <mykolazbudchak@gmail.com>
2023-03-10 10:12:35 +0200

    [update] schema updates

--- backend-service/models/users.js
+++ backend-service/models/users.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
 });
 
 module.exports = mongoose.model("User", UserSchema);
