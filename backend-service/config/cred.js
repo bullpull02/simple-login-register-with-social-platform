8b76ff117d9ad63544c3662cc3276e2faf82792d
bullpull02 <mykolazbudchak@gmail.com>
2023-03-10 10:11:55 +0200

    [add] credential addutuib

--- backend-service/config/cred.js
+++ backend-service/config/cred.js
@@ -4,13 +4,18 @@ const discord = {
     callback_url : "http://localhost:8000/api/v1/user/sessions/oauth/discord"
 }
 const google = {
-    client_id : "42056636488-5pcnca03i14e5s0747qfqjbiddluf1ng.apps.googleusercontent.com",
-    client_secret : "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",
+    client_id : "1071226693678-ckfpvtck6kirqh6m9um4a59t4984rs2d.apps.googleusercontent.com",
+    client_secret : "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",
     callback_url : "http://localhost:8000/api/v1/user/sessions/oauth/google"
 }
 const twitter = {
-    consumerKey: "TWITTER_CONSUMER_KEY",
-    consumerSecret: "TWITTER_CONSUMER_SECRET",
+    consumerKey: "KKBIawUSE_OWNPJwM",
+    consumerSecret: "BJCahJjoUSE_OWNTStPFAj",
     callbackURL: "http://localhost:8000/api/v1/user/sessions/oauth/twitter",
 }
-module.exports = {discord,google,twitter}
+const github = {
+    client_id: "c07d57a8e9fe1ea18c4d",
+    client_secret: "55c65a49ce29784c0b993e9f1e76a206f026253d",
+    callbackURL: "http://localhost:3000"
+}
+module.exports = {discord,google,twitter,github}
